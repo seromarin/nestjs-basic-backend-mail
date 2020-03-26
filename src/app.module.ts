7b7f8359113a1fac6c0ca3025a7384fea7e75489
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-03-26 09:47:15 -0500

    Login module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { LoginModule } from './login/login.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: '../db/mail.sqlite'
-    })
+    }),
+    LoginModule
   ],
   controllers: [AppController],
   providers: [AppService],
